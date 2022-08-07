1374a88e5e48ce4ce81ccfd241962269377a3b72
Tom Hume <twhume@gmail.com>
2022-08-07 07:57:07 -0700

    Arguments all plumbed in, pulling resolution from video, neater logging

--- beeflow-video.py
+++ beeflow-video.py
@@ -1,5 +1,6 @@
 # Processes and reports on a video input
 
+import argparse
 import csv
 import cv2 as cv
 import numpy as np
@@ -10,78 +11,116 @@ fontScale = 1
 thickness = 2
 color = (255, 0, 0)
 
-cap = cv.VideoCapture("sonoma.mp4")
-#cap = cv.VideoCapture("v2.mp4")
+def process_video(in_fn, out_csv_fn, output_video, edge_size, max_frames, preview, preserve_fps):
+	cap = cv.VideoCapture(in_fn)
 
-# Determine FPS
+	# Determine FPS
 
-ret, first_frame = cap.read()
-fps = cap.get(cv.CAP_PROP_FPS)
-ms_per_frame = 1000/fps
-print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+	ret, first_frame = cap.read()
+	fps = cap.get(cv.CAP_PROP_FPS)
+	ms_per_frame = 1000/fps
 
-fourcc = cv.VideoWriter_fourcc(*'MP4V')
-out = cv.VideoWriter('mumbai-320p-processed.mp4', fourcc, fps, (640,360))
+	length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+	if (max_frames is not None) and (max_frames > length):
+		print(f"Not enough frames: max_frames={max_frames}, length={length}")
+
+	if (output_video is not None):
+		fourcc = cv.VideoWriter_fourcc(*'MP4V')
+		out = cv.VideoWriter(output_video, fourcc, fps, (640,360))
+
+	v_width  = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+	v_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+	print(f"Input video resolution={v_width}x{v_height}, {length} frames at {fps:.2f} fps")
+
+	cur_time = last_time = time.time()
+
+	num = 0
+	with open(out_csv_fn, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile,  quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(["Frame","Left", "Right", "Top", "Bottom"])
+
+		while(cap.isOpened()):
+			# ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
+			ret, frame = cap.read()
+
+			if (not ret):
+				break;
+
+			if (max_frames is not None) and (num >= max_frames):
+				break;
+
+			# Convert to black and white
+
+			gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+			(thresh, im_bw) = cv.threshold(gray, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+			# Add a low pass filter to blur the image
+
+			kernel = np.array([[1, 1, 1, 1, 1], 
+				[1, 1, 1, 1, 1], 
+				[1, 1, 1, 1, 1], 
+				[1, 1, 1, 1, 1], 
+				[1, 1, 1, 1, 1]])
+			kernel = kernel/sum(kernel)
+			filtered = cv.filter2D(gray,-1,kernel)
+
+			backtorgb = cv.cvtColor(filtered,cv.COLOR_GRAY2RGB)
+
+			# pull out average values at each edge
+
+			left = np.average(filtered[:,0:edge_size])
+			right = np.average(filtered[:,(v_width-edge_size):v_width])
+			top = np.average(filtered[0:edge_size,:])
+			bottom = np.average(filtered[(v_height-edge_size):v_height,:])
+			output = backtorgb
+
+			if (preview or (output_video is not None)):
+				output = cv.putText(output, f"Left={left:.2f}" , (30,180), font, fontScale, color, thickness, cv.LINE_AA)
+				output = cv.putText(output, f"Right={right:.2f}", (420,180), font, fontScale, color, thickness, cv.LINE_AA)
+				output = cv.putText(output, f"Top={top:.2f}", (200,30), font, fontScale, color, thickness, cv.LINE_AA)
+				output = cv.putText(output, f"Bottom={bottom:.2f}", (200,330), font, fontScale, color, thickness, cv.LINE_AA)
+				output = cv.putText(output, f"f={num}", (280,180), font, fontScale, color, thickness, cv.LINE_AA)
+
+			if (preview):
+				cv.imshow("beeflow preview", output)
+				# Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
+				if cv.waitKey(10) & 0xFF == ord('q'):
+					break
+
+			if (output_video is not None):
+				out.write(output)
+
+			# Sleep to preserve input FPS in output
+			if (preserve_fps):
+				cur_time = time.time()
+				frame_time = (cur_time - last_time)
+				time.sleep((ms_per_frame - frame_time)/1000)
+				last_time = cur_time
+
+			num += 1
+			writer.writerow([num, left, right, top, bottom])
+
+		# The following frees up resources and closes all windows
+		cap.release()
+		if (output_video is not None):
+			out.release()
+		cv.destroyAllWindows()
 
 
-cur_time = last_time = time.time()
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input", type=str)
+	parser.add_argument("output_csv", type=str)
+	parser.add_argument("--output_video", type=str, default=None)
+	parser.add_argument("--edge_size", type=int, default=5)
+	parser.add_argument("--max_frames", type=int, default=None)
+	parser.add_argument("--preview", action=argparse.BooleanOptionalAction)
+	parser.add_argument("--preserve_fps", action=argparse.BooleanOptionalAction)
+	args = parser.parse_args()
+	process_video(args.input, args.output_csv, args.output_video, args.edge_size,
+		args.max_frames, args.preview, args.preserve_fps)
 
-num = 0
-with open('beeflow.csv', 'w', newline='') as csvfile:
-	writer = csv.writer(csvfile,  quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(["Frame","Left", "Right", "Top", "Bottom"])
 
-	while(cap.isOpened()):
-		# ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
-		ret, frame = cap.read()
-
-		# Convert to black and white
-
-		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		(thresh, im_bw) = cv.threshold(gray, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-
-		# Add a low pass filter
-
-		#prepare the 5x5 shaped filter
-		kernel = np.array([[1, 1, 1, 1, 1], 
-			[1, 1, 1, 1, 1], 
-			[1, 1, 1, 1, 1], 
-			[1, 1, 1, 1, 1], 
-			[1, 1, 1, 1, 1]])
-		kernel = kernel/sum(kernel)
-
-		#filter the source image
-		filtered = cv.filter2D(gray,-1,kernel)
-		backtorgb = cv.cvtColor(filtered,cv.COLOR_GRAY2RGB)
-
-		left = np.average(filtered[:,0:5])
-		right = np.average(filtered[:,635:640])
-		top = np.average(filtered[0:5,:])
-		bottom = np.average(filtered[355:360,:])
-
-		output = backtorgb
-		output = cv.putText(output, f"Left={left:.2f}" , (30,180), font, fontScale, color, thickness, cv.LINE_AA)
-		output = cv.putText(output, f"Right={right:.2f}", (420,180), font, fontScale, color, thickness, cv.LINE_AA)
-		output = cv.putText(output, f"Top={top:.2f}", (200,30), font, fontScale, color, thickness, cv.LINE_AA)
-		output = cv.putText(output, f"Bottom={bottom:.2f}", (200,330), font, fontScale, color, thickness, cv.LINE_AA)
-		output = cv.putText(output, f"f={num}", (280,180), font, fontScale, color, thickness, cv.LINE_AA)
-
-		cv.imshow("sparse optical flow", output)
-		out.write(output)
-		# Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
-		if cv.waitKey(10) & 0xFF == ord('q'):
-			break
-
-		# Sleep to preserve input FPS in output
-#		cur_time = time.time()
-#		frame_time = (cur_time - last_time)
-#		time.sleep((ms_per_frame - frame_time)/1000)
-#		last_time = cur_time
-
-		num += 1
-		writer.writerow([num, left, right, top, bottom])
-	# The following frees up resources and closes all windows
-	cap.release()
-	out.release()
-	cv.destroyAllWindows()
-
+if __name__ == "__main__":
+    main()
