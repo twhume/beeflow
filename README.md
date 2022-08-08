b2d44edfc350f279ac4ddba23036a2c604ac95ea
Tom Hume <twhume@gmail.com>
2022-08-07 17:15:26 -0700

    Added arguments and documented

--- README.md
+++ README.md
@@ -4,11 +4,7 @@ Implementation of the non-directional speed detector described in "How Bees Expl
 
 # TODO
 
-- implement arguments
-- test on more footage
-- better averaging - take a moving average not a chunk at a time
-- plot the difference in R/L speed per frame (which should tell a bee its horizontal position)
-- paste this into the movie
+- make a single script which analyzes a movie and pastes results into it (bonus points: gradually displaying graph). paste this into the movie
 
 # Process
 
@@ -16,7 +12,6 @@ Implementation of the non-directional speed detector described in "How Bees Expl
 2. Spatially low-pass filter, to give an image in which abrupt edges have been converted into ramps of constant slope
 3. Measure speed of image by measuring rate of change of response at the ramps. One image generates a train of pulses, one at each edge. Amplitude of each pulse is proportional to the rate of change of intensity at the corresponding ramp, and thus to the speed of the image at that location.
 4. Use rectification to ensure the response is positive regardless of direction of movement.
-
 
 # Videos
 
@@ -35,10 +30,29 @@ sonoma.mp4 - some footage I shot walking along a tunnel of trees in sonoma
 
 # Usage
 
-Turn a video into a CSV logging the average edge-values at each frame:
+Turn a video into a CSV logging the average edge-values at each frame to a CSV file
 
-python beeflow-video.py input.mp4 output.csv --edgesize=1 --maxframes=10000 --preview=true --preservefps=false
+python beeflow-video.py input.mp4 output.csv --edge_size=1 --max_frames=10000 --preview|no-preview --preserve_fps|no-preserve_fps --output_video=debug.mp4
+
+Parameters:
+(required) input.mp4 : input video to read from. Size, FPS should be inferred automatically.
+(required output.csv : output CSV file to write. Existing files will be overwritten
+edge_size : how many pixels in from the edge should be averaged together to infer "what's at the egde"?
+max_frames : maximum number of video frames to read
+preview/no-preview : controls whether a visible preview is shown
+preserve_fps/no-preserve_fps : should the FPS of the input be preserved when showing the preview?
+output_video : if set, preview is written to this file
+
 
 Take one of these CSV files and plot the average speed of pulses at left and right edges:
 
-python beeflow-csv.py input.csv --startframe=1 --endframe=1000
+python beeflow-csv.py input.csv --frame_start=1 --frame_end=1000 --fps=30 --prominence=10 --window_size=90
+
+(required) input.csv : CSV file created by beeflow-video.csv
+frame_start : what frame should processing start at?
+frame_end : what frame should processing end at?
+fps : fps of the original video (used to create a grap with an x axis in seconds)
+prominence : how "prominent" should peaks be when looking for them?
+window_size : over what window size (in frames) should we average inferred speed when creating a graph
+output_file : if present, write the output graph to a file, otherwise preview
+
--- beeflow-csv.py
+++ beeflow-csv.py
@@ -1,31 +1,20 @@
-# 
+# Takes the CSV file from beeflow-video.py and identifies difference in edge
+# speed between LHS and RHS, graphing them
 
+import argparse
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import find_peaks
 
-right = []
-left = []
-top = bottom = []
-max = 0
-with open('sonoma.csv', 'r', newline='') as csvfile:
-	reader = csv.reader(csvfile)
-	next(reader, None)  # skip the headers
-	for row in reader:
-		max = row[0]
-		left.append(float(row[1]))
-		right.append(float(row[2]))
-		top.append(float(row[3]))
-		bottom.append(float(row[4]))
-
-def calc_speeds(l, pad_to):
+def calc_speeds(l, pad_to, prominence=10):
 
 	l = np.array(l)
+
 	neg_l = 255 - l
 
-	l_peaks, _ = find_peaks(l, prominence=(10, ))
-	l_troughs, _ = find_peaks(neg_l, prominence=(10, )) 
+	l_peaks, _ = find_peaks(l, prominence=(prominence, ))
+	l_troughs, _ = find_peaks(neg_l, prominence=(prominence, )) 
 
 	points = np.concatenate((l_peaks, l_troughs))
 	points.sort()
@@ -40,7 +29,6 @@ def calc_speeds(l, pad_to):
 		end_val = l[end_frame]
 
 		speed = abs((end_val - start_val) / (end_frame - start_frame))
-		print("From frame", start_frame, "to frame", end_frame, "speed=", speed)
 		speeds.extend([speed] * (end_frame-start_frame))
 
 	if (len(speeds)<pad_to):
@@ -55,7 +43,8 @@ def average_speed(l, frames=30):
 	out = []
 	for i in range(len(l)):
 		num_back = min(frames, i+1)
-		out.append(sum(l[i-num_back:i]) / num_back)
+		start=max(0, i-num_back)
+		out.append(sum(l[start:i+1])/num_back)
 	return out
 
 def to_seconds(l, fps=30):
@@ -64,56 +53,69 @@ def to_seconds(l, fps=30):
 		ret.append(sum(c)/fps)
 	return np.array(ret)
 
-#left = left[:3000]
-#right = right[:3000]
-print("l=",len(left))
-print("r=",len(right))
+def process_csv(input_csv, output_file, prominence, fps, window_size, frame_start, frame_end):
 
-l_speeds = calc_speeds(left, 0)
-r_speeds = calc_speeds(right, len(l_speeds))
+	# Read in the CSV file made by beeflow-video.py
 
-print("l_speeds=",len(l_speeds))
-print("r_speeds=",len(r_speeds))
+	right = []
+	left = []
+	with open(input_csv, 'r', newline='') as csvfile:
+		reader = csv.reader(csvfile)
+		next(reader, None)  # skip the headers
+		for row in reader:
+			left.append(float(row[1]))
+			right.append(float(row[2]))
+
+	# cut out 
+
+	if (frame_end is not None):
+		left = left[:frame_end]
+		right = right[:frame_end]
+
+	if (frame_start is not None):
+		left = left[frame_start:]
+		right = right[frame_start:]
+
+	l_speeds = calc_speeds(left, 0, prominence=prominence)
+	r_speeds = calc_speeds(right, len(l_speeds), prominence=prominence)
+	if len(r_speeds) > len(l_speeds):
+		pad_from = len(l_speeds)
+		l_speeds.resize(len(r_speeds))
+		l_speeds[pad_from:] = l_speeds[pad_from]
+
+	l_av = to_seconds(average_speed(l_speeds, frames=window_size), fps=fps)
+	r_av = to_seconds(average_speed(r_speeds, frames=window_size), fps=fps)
+
+	max_av = np.max([np.amax(l_av), np.amax(r_av)])
+	min_av = np.min([np.amin(l_av), np.amin(r_av)])
+
+	plt.title("L/R position by second")
+	plt.plot((r_av-l_av)/(max_av-min_av))
+	plt.ylim((-1,1))
+	plt.xlabel("Time (s)")
+	plt.ylabel("Position (L=-1, R=1)")
+
+	if (output_file is not None):
+		plt.savefig(output_file)
+	else:
+		plt.show()
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_csv", type=str)
+	parser.add_argument("--output_file", type=str)
+	parser.add_argument("--prominence", type=int, default=5)
+	parser.add_argument("--fps", type=int, default=30)
+	parser.add_argument("--window_size", type=int, default=90)
+	parser.add_argument("--frame_start", type=int, default=0)
+	parser.add_argument("--frame_end", type=int, default=None)
+
+	args = parser.parse_args()
+
+	process_csv(args.input_csv, args.output_file, args.prominence, args.fps, args.window_size,
+		args.frame_start, args.frame_end)
 
 
-l_av = to_seconds(average_speed(l_speeds, frames=30))
-r_av = to_seconds(average_speed(r_speeds, frames=30))
-
-print("l_av=",len(l_av))
-print("r_av=",len(r_av))
-
-max_av = np.max([np.amax(l_av), np.amax(r_av)])
-min_av = np.min([np.amin(l_av), np.amin(r_av)])
-
-
-
-print(len(l_av))
-print(len(r_av))
-plt.title("L/R position by second")
-#plt.xlim(left=1)
-#plt.plot(l_av)
-#plt.plot(r_av)
-plt.plot((r_av-l_av)/(max_av-min_av))
-plt.ylim((-1,1))
-plt.show()
-
-#plt.plot(left)
-#plt.title("Intensity of left and right frame edges")
-#plt.plot(left, label="Left")
-#plt.plot(right, label="right")
-#plt.xlabel("Frame")
-#plt.ylabel("Intensity")
-#plt.legend()
-#plt.show()
-
-
-
-
-#plt.plot(l_peaks, left[l_peaks], "x")
-#plt.plot(r_peaks, right[r_peaks], "o")
-#plt.plot(l_troughs, left[l_troughs], "o")
-#plt.plot(r_peaks, right[r_peaks], "o")
-#plt.plot(r_peaks)
-#plt.show()
-
+if __name__ == "__main__":
+    main()
 
